f7ac59d27854491a957b3b1c6f35bd7a1310fb5f
Krzysztof Bartczak <krzysztof@bartczak.eu>
2020-09-28 19:39:12 +0200

    MariaDB remove user name secret key

--- pi/mariadb.sh
+++ pi/mariadb.sh
@@ -11,7 +11,6 @@ docker rm ${NAME}
 docker create \
    --name=${NAME} \
    -e MYSQL_ROOT_PASSWORD=/run/secrets/${NAME}/root_password \
-   -e MYSQL_USER=/run/secrets/${NAME}/root_user \
    -v /media/docker/storage/${NAME}:/var/lib/mysql \
    ${IMAGE}
 
