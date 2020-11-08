8a03f4f66ec667050e53eadfb74cb14e9cfee7d1
Krzysztof Bartczak <krzysztof@bartczak.eu>
2020-11-08 17:54:04 +0100

    domoticz

---
+++ pi/domoticz.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+readonly NAME="domoticz"
+readonly IMAGE="linuxserver/domoticz"
+
+docker pull ${IMAGE}
+
+docker stop ${NAME}
+docker rm ${NAME}
+
+docker create \
+  --name=${NAME} \
+  -e TZ=Europe/London \
+  -e VIRTUAL_PORT=9117 \
+  -e PUID=1000 -e PGID=1000 \
+  -e TZ=Europe/London \
+  -p 9180:8080 \
+  -p 6144:6144 \
+  -p 1443:1443 \
+  -v /media/docker/${NAME}/config:/config \
+  --restart unless-stopped \
+  ${IMAGE}
+
+docker start ${NAME}
