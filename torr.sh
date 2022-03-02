60eb6817daaf682137fa3d0b93885d3e4f7f5a5b
Krzysztof Bartczak <k.bartczak@marketlab.pl>
2022-03-02 11:01:35 +0100

    flexget in sh:wq

---
+++ torr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run -d \
+    --name=fget \
+    -p 5050:5050 \
+    -v $(pwd)/fget/data:/data \
+    -v $(pwd)/fget/config:/config \
+    -e FG_WEBUI_PASSWD=qweqwe \
+    -e FG_LOG_LEVEL=info \
+    -e FG_LOG_FILE=flexget.log \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -e TZ=Europe/London \
+    wiserain/flexget
